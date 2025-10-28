443edf4f770a6cd16938aee8f17d88e96e6f7c79
IvieThePlant <ivietheplant@outlook.com>
2025-10-28 09:41:09 -0500

    applying Brooks' idea

---
+++ lib/arux_app/api/support_portal.rb
@@ -0,0 +1,28 @@
+module AruxApp
+  module API
+    class SupportPortal
+      DOMAINS = {
+        production: "arux.blue",
+        staging: "arux.blue",
+        development: HOSTNAME,
+        test: "arux.test" # do we really need this?
+      }
+
+      def self.public_uri
+        AruxApp::API.uri(subdomain: "support-portal")
+      end
+
+      def public_uri
+        self.class.public_uri
+      end
+
+      def self.api_uri
+        AruxApp::API.uri(subdomain: "support-portal.api")
+      end
+
+      def api_uri
+        self.class.api_uri
+      end
+    end
+  end
+end
