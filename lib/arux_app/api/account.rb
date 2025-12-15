f711b129e1fa418a7ba8ce4b02548d00e826fb69
Chris Warren <chris.warren@aruxsoftware.com>
2025-12-15 14:56:03 -0600

    swap Faraday in for HTTPI in account.rb

--- lib/arux_app/api/account.rb
+++ lib/arux_app/api/account.rb
@@ -30,120 +30,96 @@ module AruxApp
       end
 
       def list(params = {})
-        request = HTTPI::Request.new
-        request.url = "#{api_route}/users"
-        request.query = URI.encode_www_form(params)
-        request.headers = self.generate_headers
-
-        response = HTTPI.get(request)
-
-        if !response.error?
+        conn = Faraday.new(url: api_route) do |f|
+          f.headers = self.generate_headers
+        end
+        response = conn.get("/users?#{URI.encode_www_form(params)}")
+        if response.status < 400
           JSON.parse(response.body)
         else
-          raise(API::Error.new(response.code, response.body))
+          raise(API::Error.new(response.status, response.body))
         end
       end
 
       def get(uuid, params = {})
         uuid = AruxApp::API.uri_escape(uuid.to_s)
-
-        request = HTTPI::Request.new
-        request.url = "#{api_route}/users/#{uuid}"
-        request.query = URI.encode_www_form(params)
-        request.headers = self.generate_headers
-
-        response = HTTPI.get(request)
-
-        if !response.error?
+        conn = Faraday.new(url: api_route) do |f|
+          f.headers = self.generate_headers
+        end
+        response = conn.get("/users/#{uuid}?#{URI.encode_www_form(params)}")
+        if response.status < 400
           JSON.parse(response.body)
         else
-          raise(API::Error.new(response.code, response.body))
+          raise(API::Error.new(response.status, response.body))
         end
       end
 
       def create(params)
-        request = HTTPI::Request.new
-        request.url = "#{api_route}/users/"
-        request.body = params.to_json
-        request.headers = self.generate_headers
-
-        response = HTTPI.post(request)
-
-        if response.code == 201
+        conn = Faraday.new(url: api_route) do |f|
+          f.headers = self.generate_headers
+        end
+        response = conn.post("/users/", params.to_json)
+        if response.status == 201
           true
-        elsif !response.error?
+        elsif response.status < 400
           JSON.parse(response.body)
         else
-          raise(API::Error.new(response.code, response.body))
+          raise(API::Error.new(response.status, response.body))
         end
       end
 
       def update(uuid, params)
         uuid = AruxApp::API.uri_escape(uuid.to_s)
-
-        request = HTTPI::Request.new
-        request.url = "#{api_route}/users/#{uuid}"
-        request.body = params.to_json
-        request.headers = self.generate_headers
-
-        response = HTTPI.put(request)
-
-        if response.code == 204
+        conn = Faraday.new(url: api_route) do |f|
+          f.headers = self.generate_headers
+        end
+        response = conn.put("/users/#{uuid}", params.to_json)
+        if response.status == 204
           true
-        elsif !response.error?
+        elsif response.status < 400
           JSON.parse(response.body)
         else
-          raise(API::Error.new(response.code, response.body))
+          raise(API::Error.new(response.status, response.body))
         end
       end
 
       def merge(uuid1, uuid2)
         uuid1 = AruxApp::API.uri_escape(uuid1)
         uuid2 = AruxApp::API.uri_escape(uuid2)
-
-        request = HTTPI::Request.new
-        request.url = "#{api_route}/users/merge/#{uuid1}/#{uuid2}"
-        request.headers = self.generate_headers
-
-        response = HTTPI.put(request)
-
-        if !response.error?
+        conn = Faraday.new(url: api_route) do |f|
+          f.headers = self.generate_headers
+        end
+        response = conn.put("/users/merge/#{uuid1}/#{uuid2}")
+        if response.status < 400
           JSON.parse(response.body)
         else
-          raise(API::Error.new(response.code, response.body))
+          raise(API::Error.new(response.status, response.body))
         end
       end
 
       def delete(uuid)
         uuid = AruxApp::API.uri_escape(uuid.to_s)
-
-        request = HTTPI::Request.new
-        request.url = "#{api_route}/users/#{uuid}"
-        request.headers = self.generate_headers
-
-        response = HTTPI.delete(request)
-
-        if !response.error?
+        conn = Faraday.new(url: api_route) do |f|
+          f.headers = self.generate_headers
+        end
+        response = conn.delete("/users/#{uuid}")
+        if response.status < 400
           JSON.parse(response.body)
         else
-          raise(API::Error.new(response.code, response.body))
+          raise(API::Error.new(response.status, response.body))
         end
       end
 
       def owner(params = {})
         raise API::RequirementError.new(:access_token, "can't be blank") if self.access_token.nil?
-
-        request = HTTPI::Request.new
-        request.url = "#{api_route}/users/owner"
-        request.query = URI.encode_www_form(params)
-        request.headers = self.generate_headers
-
-        response = HTTPI.get(request)
-
-        if !response.error?
+        conn = Faraday.new(url: api_route) do |f|
+          f.headers = self.generate_headers
+        end
+        response = conn.get("/users/owner?#{URI.encode_www_form(params)}")
+        if response.status < 400
           JSON.parse(response.body)
         else
-          raise(API::Error.new(response.code, response.body))
+          raise(API::Error.new(response.status, response.body))
         end
       end
 
